23fca98bd6a50824ff253739547426e9476f9782
Vedant <vedantgire48@gmail.com>
2022-10-13 22:10:38 +0530

    Combination Optimized program

---
+++ combination.py
@@ -0,0 +1,12 @@
+n = 5
+
+result = 1
+
+for i in range(5):
+    result *= (n - i)
+    result //= (i + 1)
+print(result)
+
+
+#Optimized technique to find Combinations
+# Used to find pascal traingle row
